4f1c0a774ccfeca7050cd400169a7a5b6aacc086
Noah Syrkis <noah@syrkis.com>
2023-09-02 10:52:53 -0300

    form test

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -7,6 +7,26 @@
 
 	let nImages = data.images.length;
 
+	const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+  
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
+
+document
+  .querySelector("form")
+  .addEventListener("submit", handleSubmit);
+
+
 </script>
 
 <div class="container">
